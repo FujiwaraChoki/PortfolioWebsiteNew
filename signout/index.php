13a904cf058503398b780b2b7b68165b84e253d7
Sami Hindi <lolipop66611@gmail.com>
2022-06-08 22:02:52 +0200

    Added bunch of neccessary stuff in index.php at signout

--- signout/index.php
+++ signout/index.php
@@ -1,5 +1,13 @@
 <?php
     session_start();
-    unset($_SESSION['loggedIn']);
-    header("Location: ../index.php");
+    $con = new mysqli('sql304.epizy.com', 	'epiz_31910323', 'K3FXEzNspxk9i', 'epiz_31910323_User');
+    if($con->connect_error) {
+        die('Failed to connect : ' . $con->connect_error);
+    } else {
+        $stmt = $con->prepare("update Grade set loggedIn = 0");
+        $stmt->execute();
+        $stmt->close();
+        $con->close();
+    }
+    echo '<p>Added Grade!</p>';
 ?>
