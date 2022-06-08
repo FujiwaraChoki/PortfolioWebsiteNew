c08b8923928bac0108ba67aa0eb8f70b94c11989
Sami Hindi <lolipop66611@gmail.com>
2022-06-08 20:09:54 +0200

    Changes made in School

--- notenberechner/addGrade/addGradeMind.php
+++ notenberechner/addGrade/addGradeMind.php
@@ -1,4 +1,16 @@
 <?php
     session_start();
     $grade = $_POST['grade'];
+    $userid = $_SESSION['UserID'];
+
+    $con = new mysqli('sql304.epizy.com', 'epiz_31910323', 'K3FXEzNspxk9i', 'Grade');
+    if($con->connect_error) {
+        die('Failed to connect : ' . $con->connect_error);
+    } else {
+        $stmt = $con->prepare("insert into Grade(UserID, Grade) values(?, ?)");
+        $stmt->bind_param("is", $userid, $grade);
+        $stmt->execute();
+        $stmt->close();
+        $con->close();
+    }
 ?>
